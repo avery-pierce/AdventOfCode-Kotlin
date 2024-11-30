342908e2c30d7e4792e68706bb3a131e19fcfacd
Avery Pierce <avery.pierce@protonmail.com>
2024-11-30 15:57:51 -0600

    Add puzzle templates for 2024

---
+++ src/puzzles/Puzzle2403.kt
@@ -0,0 +1,25 @@
+package puzzles
+
+import utils.*
+
+class Puzzle2403: Puzzle() {
+    override fun solveA(input: Input): Any {
+        return super.solveA(input)
+    }
+
+    override val testCasesA: List<TestCase>
+        get() = super.testCasesA
+
+    override fun solveB(input: Input): Any {
+        return super.solveB(input)
+    }
+
+    override val testCasesB: List<TestCase>
+        get() = super.testCasesB
+}
+
+fun main() {
+    val p = Puzzle2403()
+    val text = object {}.javaClass.getResource("/.txt")?.readText()
+    p.run(text!!)
+}
